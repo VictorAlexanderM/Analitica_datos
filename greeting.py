65f6e009873f5069bb04077f17c708ac0e003459
Victor Martin <victoralexmartin@gmail.com>
2024-10-30 19:24:13 -0500

    test 1

--- greeting.py
+++ greeting.py
@@ -1,7 +1,7 @@
 
 # greeting.py - A simple script to greet the user
 def greet():
-    print("Hello! Welcome to this Python script.")
+    print("Hello! Welcome to this Python script")
 
 if __name__ == "__main__":
     greet()
